d649dd87856adc197d138a0019269e929844c847
OfficialBradisswag <bradleynz@icloud.com>
2018-05-27 13:51:10 +1200

    first commit

---
+++ DiscordBotCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscordBotCore
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
